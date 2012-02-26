a2d481045a14399a908ead95be004a59d9a493cd
Toon Verwaest <toon.verwaest@gmail.com>
2012-02-26 16:32:20 +0100

    use more constants

--- l6.c
+++ l6.c
@@ -4,11 +4,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define TIMEOUT  10
-#define MAXINPUT 1024
-#define PIPEMAX  (1<<16)
-#define MSGLEN   33 // Welcome...\n
-#define TAUNTLEN 35 // Haha...\n
+#define TIMEOUT     10
+#define MAXINPUT    1024
+#define PIPEMAX     (1<<16)
+#define MSGLEN      33 // Welcome...\n
+#define TAUNTLEN    35 // Haha...\n
+#define CHAR_START  1
+#define CHAR_END    128
 
 char        filling[PIPEMAX], buf[PIPEMAX];
 const char  *prog, *file;
@@ -39,13 +41,14 @@ int main(int argc, char ** argv) {
         fprintf(stderr, "Usage: %s PROG FILE\n", argv[0]);
         exit(-1);
     }
+
     prog = argv[1];
     file = argv[2];
     oldout = dup(STDOUT_FILENO);
     pipe(p_out);
-    dup2(p_out[1], 1);
+    dup2(p_out[1], STDOUT_FILENO);
     pipe(p_err);
-    dup2(p_err[1], 2);
+    dup2(p_err[1], STDERR_FILENO);
     poll_out.fd = p_out[0];
     poll_out.events = POLLIN;
     memset(filling, 'a', PIPEMAX);
@@ -53,10 +56,10 @@ int main(int argc, char ** argv) {
     char input[MAXINPUT] = {0};
     int size;
     unsigned char c;
-    for (size = 0; c < 128; size++) {
+    for (size = 0; c < CHAR_END; size++) {
         // fill in extra char to make sure we'll fail
         input[size] = -1;
-        for (c = 1; c < 128; c++) {
+        for (c = CHAR_START; c < CHAR_END; c++) {
             input[size-1] = c;
             if (test_string(size, input)) {
                 break;
@@ -66,6 +69,6 @@ int main(int argc, char ** argv) {
 
     // make it print the pwd to stdout
     input[size-2] = '\0';
-    dup2(oldout, 2);
+    dup2(oldout, STDERR_FILENO);
     execl(prog, prog, file, input, NULL);
 }
